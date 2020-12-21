8a5f98ced2514f6ad958eed6de093f46847289e5
Abhinav Dutt <abhinavdutt19@gmail.com>
2020-12-22 03:48:47 +0800

    Time: 0 ms (100.00%), Space: 6.5 MB (30.46%) - LeetHub

---
+++ permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> fact;
+        fact.push_back(1);
+        for(int i=1;i<=9;i++)
+        {   
+            fact.push_back(fact[i-1]*i);
+        }
+        string digits = "123456789";
+        std::string ans="";
+        k--;
+​
+        for(int i=n; i>0; i--)
+        {
+            int index = k/fact[i-1];
+            k = k%fact[i-1];
+            ans += digits[index];
+            digits.erase(digits.begin()+index);
+        }
+        return ans;
+    }
+};
