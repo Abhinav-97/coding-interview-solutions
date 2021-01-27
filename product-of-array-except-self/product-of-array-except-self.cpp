c80e9001a341e4aa50a3f5d73247c6ef1735da03
Abhinav Dutt <abhinavdutt19@gmail.com>
2021-01-27 23:39:28 +0530

    Time: 88 ms (5.72%), Space: 26 MB (8.13%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> leftProducts(nums.size()), rightProducts(nums.size());
-        int leftProduct = 1;
-        int rightProduct = 1;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i > 0)
-            {
-                leftProduct *= nums[i-1];
-                leftProducts[i] = leftProduct;
-            }
-            else{
-                leftProducts[i] = leftProduct;
-            }
-        }
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            if(i < nums.size()-1)
-            {
-                rightProduct *= nums[i+1];
-                rightProducts[i] = rightProduct;
-            }
-            else{
-                rightProducts[i] = rightProduct;
-            }
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            cout<<rightProducts[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<nums.size();i++)
-        {
-            cout<<leftProducts[i]<<" ";
-        }
-        vector<int> ans;
-        for(int i = 0;i<nums.size();i++)
-        {
-            ans.push_back(leftProducts[i]*rightProducts[i]);
-        }
-        return ans;
-        
-        
-    }
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> leftProduct(nums.size());
+        vector<int> rightProduct(nums.size());
+        
+        leftProduct[0] = 1;
+        rightProduct[nums.size()-1] = 1;
+        for(int i=1;i<nums.size();i++)
+        {
+            leftProduct[i] = nums[i-1]*leftProduct[i-1];
+        }
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            rightProduct[i] = nums[i+1]*rightProduct[i+1];
+        }
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {   cout<<leftProduct[i]<<" "<<rightProduct[i]<<endl;
+            ans.push_back(leftProduct[i]*rightProduct[i]);
+        }
+        return ans;
+    }
 };
