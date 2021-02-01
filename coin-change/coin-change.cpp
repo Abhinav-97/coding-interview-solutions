91dfaab30a2cd337198bc9978f9518cf08b3f07c
Abhinav Dutt <abhinavdutt19@gmail.com>
2021-02-02 01:20:35 +0530

    Time: 80 ms (72.90%), Space: 14.2 MB (57.24%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int dp[amount+1];
-        memset(dp, 0, sizeof(dp));
-        for(int val=1;val<=amount;val++)
-        {
-            int minWays = INT_MAX;
-            for(int i=0;i<coins.size();i++)
-            {
-                if(val >= coins[i] && dp[val-coins[i]] != -1)
-                {
-                    minWays = min(minWays, dp[val - coins[i]] + 1);
-                }
-            }
-            if(minWays == INT_MAX)
-            {
-                dp[val] = -1;
-            }
-            else{
-                dp[val] = minWays;
-            }
-        }
-        return dp[amount];
-    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, 0);
+        int ans = coinChangeDP(coins, amount, dp);
+        return ans;
+    }
+    int coinChangeDP(vector<int>& coins, int amount, vector<int> &dp)
+    {
+        if(amount == 0)
+        {
+            return 0;
+        }
+        if(amount < 0)
+        {
+            return INT_MAX;
+        }
+        if(dp[amount] != 0)
+        {
+            return dp[amount];
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<coins.size();i++)
+        {
+            int ways = coinChangeDP(coins, amount-coins[i], dp);
+            if(ways != INT_MAX && ways != -1 && ways+1 < ans)
+            {
+                ans = ways+1;
+            }
+        }
+        // cout<<ans<<" ";
+        if(ans == INT_MAX)
+        {
+            return dp[amount] = -1;
+        }
+        return dp[amount] = ans;
+    }
 };
