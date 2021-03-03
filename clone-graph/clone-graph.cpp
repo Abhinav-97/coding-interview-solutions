faf0f4313ea5ad9e7ee6ac074a0650e7bd3fe3eb
Abhinav Dutt <abhinavdutt19@gmail.com>
2021-03-04 00:56:46 +0530

    Time: 4 ms (97.08%), Space: 9 MB (28.11%) - LeetHub

--- clone-graph/clone-graph.cpp
+++ clone-graph/clone-graph.cpp
@@ -2,51 +2,50 @@
 // Definition for a Node.
 class Node {
 public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 };
 */
-​
+
 class Solution {
 public:
-    
-    Node* cloneGraph(Node* node) {
-        unordered_map<Node*, Node*> mp;
-        dfs(node, mp);
-        return mp[node];
-    }
-    void dfs(Node* node, unordered_map<Node*, Node*> &mp)
-    {
-        if(node == NULL)
-        {
-            return;
-        }
-        if(mp[node] != NULL)
-        {
-            return;
-        }
-        Node* cloneNode = new Node(node->val);
-        mp[node] = cloneNode;
-        vector<Node*> vec = node->neighbors;
-        for(int i=0;i<vec.size();i++)
-        {
-            if(mp.find(vec[i]) == mp.end())
-            {
-                dfs(vec[i], mp);
-            }
-            mp[node]->neighbors.push_back(mp[vec[i]]);
-        }
-    }
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> mp;
+        dfs(node, mp);
+        return mp[node];
+    }
+    
+    void dfs(Node* node, unordered_map<Node*, Node*> &mp)   
+    {
+        if(node == NULL)
+        {
+            return;
+        }
+        if(mp[node] != NULL)
+        {
+            return;
+        }
+        Node* cloneNode = new Node(node->val);
+        mp[node] = cloneNode;
+        for(int i=0;i<node->neighbors.size();i++)
+        {
+            if(mp.find(node->neighbors[i]) == mp.end())
+            {
+                dfs(node->neighbors[i], mp);
+            }
+            mp[node]->neighbors.push_back(mp[node->neighbors[i]]);
+        }
+    }
 };
