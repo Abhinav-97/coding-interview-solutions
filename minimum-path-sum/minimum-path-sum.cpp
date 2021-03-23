6cb8ebdcbfc2b5ec9397bf618c5db6a5d0303c82
Abhinav Dutt <abhinavdutt19@gmail.com>
2021-03-23 23:37:39 +0530

    Time: 8 ms (92.47%), Space: 9.9 MB (73.08%) - LeetHub

--- minimum-path-sum/minimum-path-sum.cpp
+++ minimum-path-sum/minimum-path-sum.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        if(m == 0)
-        {
-            return 0;
-        }
-        int n = grid[0].size();
-        if(n == 0)
-        {
-            return 0;
-        }
-        int dp[m][n];
-        memset(dp, 0, sizeof(dp));
-        dp[0][0] = grid[0][0];
-        for(int i=1;i<m;i++)
-        {
-            dp[i][0] = grid[i][0] + dp[i-1][0]; 
-        }
-        for(int j=1;j<n;j++)
-        {
-            dp[0][j] = grid[0][j] + dp[0][j-1];
-        }
-        
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[m-1][n-1];
-        
-    }
+    int minPathSum(vector<vector<int>>& grid) {
+        if(grid.size() == 0 || grid[0].size() == 0)
+        {
+            return 0;
+        }
+        int dp[grid.size()][grid[0].size()];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<grid.size();i++)
+        {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for(int j=1;j<grid[0].size();j++)
+        {
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
+        for(int i=1;i<grid.size();i++)
+        {
+            for(int j=1;j<grid[0].size();j++)
+            {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[grid.size()-1][grid[0].size()-1];
+    }
 };
