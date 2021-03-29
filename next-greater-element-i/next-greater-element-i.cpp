edebc2c6c035e7ec99aa7ed84ef73dbacac9c51b
Abhinav Dutt <abhinavdutt19@gmail.com>
2021-03-29 19:42:48 +0530

    Time: 8 ms (56.05%), Space: 8.7 MB (71.73%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int, int> greaterElementMap;
-        stack<int> st;
-        for(int i=0;i<nums2.size();i++)
-        {
-            while(!st.empty() && st.top() < nums2[i])
-            {
-                greaterElementMap[st.top()] = nums2[i];
-                st.pop();
-            }
-            greaterElementMap[nums2[i]] = -1;
-            st.push(nums2[i]);
-        }
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++)
-        {
-            ans.push_back(greaterElementMap[nums1[i]]);
-        }
-        return ans;
-    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;
+        stack<int> st;
+        for(int i=0;i<nums2.size();i++)
+        {
+            while(!st.empty() && st.top() < nums2[i])
+            {
+                mp[st.top()] = nums2[i];
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++)
+        {
+            if(mp.find(nums1[i]) == mp.end())
+            {
+                nums1[i] = -1;
+            }
+            else{
+                nums1[i] = mp[nums1[i]];
+            }
+        }
+        return nums1;
+    }
 };
